c49d36d75d60d1982c1b36830a8a908643af1242
Nestan <nestankeneshova16@gmail.com>
2023-04-04 16:04:31 +0600

    Added the navigation to the link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ export default function App() {
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma</p>
+
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
     </div>
   );
 }
