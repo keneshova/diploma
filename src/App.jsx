4e7d39e6ca321379f0e16b74a79d01e84bea3db3
Nestan <nestankeneshova16@gmail.com>
2023-04-06 15:00:23 +0600

    Reset CSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ export default function App() {
        <Routes>
           <Route path="/" element={<h2>Home</h2>} exact></Route>
           <Route path="/about" element={<h2>About</h2>} exact></Route>
-          <Route path="/contact" element={<h2>Contact</h2>} exact></Route>
         </Routes>
       </Layout>
 
       <main>
       </main>
+     
     </div>
   );
 }
