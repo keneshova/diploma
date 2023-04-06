9a6100b94b7014cd30f092a023d52419e605f8a2
Nestan <nestankeneshova16@gmail.com>
2023-04-06 15:06:47 +0600

    added two links called contact and delivery

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,11 @@ export default function App() {
        <Routes>
           <Route path="/" element={<h2>Home</h2>} exact></Route>
           <Route path="/about" element={<h2>About</h2>} exact></Route>
+          <Route path="/contact" element={<h2>Contact</h2>} exact></Route>
+          <Route path="/delivery" element={<h2>Delivery</h2>} exact></Route>
+          
         </Routes>
       </Layout>
-
-      <main>
-      </main>
-     
     </div>
   );
 }
