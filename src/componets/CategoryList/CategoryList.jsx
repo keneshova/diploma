d6914e49daa53b05f1e2e9078327c6edbd87cf4b
Nestan <nestankeneshova16@gmail.com>
2023-04-11 16:15:32 +0600

    added component categorieslist

--- src/componets/CategoryList/CategoryList.jsx
+++ src/componets/CategoryList/CategoryList.jsx
@@ -1,9 +1,40 @@
-import "./CategoryList.css"
-export default function CategoryList(){
-  return(
+import { NavLink } from "react-router-dom";
+import "./CategoryList.css";
+import { useEffect, useState } from "react";
+import { getDocs } from "firebase/firestore";
+import { categoryCollection } from "../../firebase";
+
+export default function CategoryList() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getDocs(categoryCollection).then((snapshot) => {
+      //категории будут хпанить в snapshot.docs
+
+      //создать массив для категорий
+      const newCategories = [];
+      //
+
+      snapshot.docs.forEach((doc) => {
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+
+      setCategories(newCategories);
+    });
+  }, []);
+
+  const output = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+    </li>
+  ));
+
+  return (
     <div className="CategoryList">
-      <ul></ul>
+      <ul>{output}</ul>
     </div>
   );
-
 }
