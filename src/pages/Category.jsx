e64e1d58764c455f667f01791bc57069fa3c4bcf
Nestan <nestankeneshova16@gmail.com>
2023-04-13 16:02:42 +0600

    changed some places and fixed bugs

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,15 +1,20 @@
 import { useContext } from "react";
 import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
+import ProductList from "../componets/ProductList/ProductList";
 
 export default function Category() {
+  //деструктизация
   const { params } = useMatch("/category/:path");
-  const { categories } = useContext (AppContext);
+  //простой метод
+  const { categories } = useContext(AppContext);
 
-  
+  const category = categories.find((category) => params.path === category.path);
+
   return (
     <div className="Category">
-      <h1>Category</h1>
+      <h1>{category ? category.name : "Loading..."}</h1>
+      <ProductList />
     </div>
   );
 }
