361c5103fa6356830390f2322b9ce4e04e7fbc0a
Nestan <nestankeneshova16@gmail.com>
2023-04-13 16:33:09 +0600

    added picture, name and price

--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -1,9 +1,22 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../../App";
 import "./ProductList.css";
 
 export default function ProductList(){
+  const { products } = useContext(AppContext);
+
+  const output = products.map(product =>(
+    <div className="Product" key={product.id}>
+      <img src={product.picture} alt={product.name} />
+      <Link to={"/product/" + product.path}>{product.name} </Link>
+      <span>{product.price} $</span>
+    </div>
+  ));
+
   return (
     <div className="ProductList">
-
+      {output}
     </div>
   );
 }
