a2b9a5c829b8d0f7f6484e755a5807345a72e1a5
Nestan <nestankeneshova16@gmail.com>
2023-04-13 14:45:09 +0600

    we cut the useState from the Categorylist and inserted it into the

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,52 @@
-import { Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Layout from "./componets/Layout/Layout";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Delivery from "./pages/Delivery";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import { createContext, useEffect, useState } from "react";
+import { getDocs } from "firebase/firestore";
+import { categoryCollection } from "./firebase";
+
+export const AppContext = createContext({
+  categories: [],
+});
 
 export default function App() {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    getDocs(categoryCollection).then((snapshot) => {
+      //категории будут хпанить в snapshot.docs
+
+      //создать массив для категорий
+      const newCategories = [];
+      //
+
+      snapshot.docs.forEach((doc) => {
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Layout>
-       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/delivery" element={<Delivery />} />
-          <Route path="/category/:path" element={<Category/>} />
-          
-        </Routes>
-      </Layout>
+      <AppContext.Provider value={{ categories }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/category/:path" element={<Category />} />
+          </Routes>
+        </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
