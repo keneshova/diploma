6fa02feb830d25c27e28e98763bffb2069fc0e58
Nestan <nestankeneshova16@gmail.com>
2023-04-18 16:06:35 +0600

    did a important from cartlist

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,10 @@
+import CartList from "../componets/CartList/CartList";
+
+export default function Cart() {
+  return (
+    <div className="Cart">
+      <h1>Cart</h1>
+      <CartList />
+    </div>
+  );
+}
