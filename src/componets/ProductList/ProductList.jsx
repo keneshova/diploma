876b974200b71b0850a761347a1e661e5931c432
Nestan <nestankeneshova16@gmail.com>
2023-04-18 14:52:55 +0600

    filter by categories

--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import { AppContext } from "../../App";
 import "./ProductList.css";
 
-export default function ProductList(){
+export default function ProductList({ category }){
   const { products } = useContext(AppContext);
 
-  const output = products.map(product =>(
+  const output = products 
+  .filter(product => product.category === category.id)
+  .map(product =>(
     <div className="Product" key={product.id}>
       <img src={product.picture} alt={product.name} />
       <Link to={"/product/" + product.path}>{product.name} </Link>
