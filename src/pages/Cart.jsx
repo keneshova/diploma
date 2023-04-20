8441f733618b55f956029902c6e36e888da913e3
Nestan <nestankeneshova16@gmail.com>
2023-04-20 16:15:33 +0600

    changed and added changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,12 @@
 import CartList from "../componets/CartList/CartList";
+import OrderForm from "../componets/OrderForm/OrderForm";
 
 export default function Cart() {
   return (
     <div className="Cart">
       <h1>Cart</h1>
       <CartList />
+      <OrderForm />
     </div>
   );
 }
