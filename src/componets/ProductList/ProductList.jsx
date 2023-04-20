1203cb7187320d1e432f8a0ec507e2797f9f0c8b
Nestan <nestankeneshova16@gmail.com>
2023-04-20 19:09:30 +0600

    added className AboutProduct

--- src/componets/ProductList/ProductList.jsx
+++ src/componets/ProductList/ProductList.jsx
@@ -4,23 +4,19 @@ import { AppContext } from "../../App";
 import AddToCart from "../AddToCart/AddToCart";
 import "./ProductList.css";
 
-export default function ProductList({ category }){
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-  const output = products 
-  .filter(product => product.category === category.id)
-  .map(product =>(
-    <div className="Product" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.path}>{product.name} </Link>
-      <span>{product.price} $</span>
-      <AddToCart product={product} />
-    </div>
-  ));
+  const output = products
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div className="Product" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link className="AboutProduct" to={"/product/" + product.path}>{product.name} </Link>
+        <span>{product.price} $</span>
+        <AddToCart product={product} />
+      </div>
+    ));
 
-  return (
-    <div className="ProductList">
-      {output}
-    </div>
-  );
+  return <div className="ProductList">{output}</div>;
 }
