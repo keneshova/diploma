df58169b762c7b549c34d5d3dcbfd52bdb58594a
Nestan <nestankeneshova16@gmail.com>
2023-04-23 16:21:40 +0600

    changed paraghraph

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ export default function Home(){
   return(
     <div className="Home">
       <h1>Welcome to Jewelry store</h1>
-      <p>Our jevelry store specializes in providing our customers with high quality, timeless jevelry that will make any look elegant. </p>
-      <p>We only</p>
+      <p>We will help you find your style and taste</p>
+  
     </div>
   );
 }
