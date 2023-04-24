c84d362984f6c9eae8eea25b22e231ba10687bfe
Nestan <nestankeneshova16@gmail.com>
2023-04-24 19:43:16 +0600

    wrote information about us in class about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,9 @@ export default function About(){
   return(
     <div className="About">
       <h1>About us</h1>
-      <p>This is </p>
-      <p>We only</p>
+      <p>JEWERLY STORE - a new classic with a modern twist. Jewrly is easy to combine with each other and suitable for any image.</p> 
+       <p>JEWERLY STORE - is so delicate, weightless and comfortable that you will forget that you are wearing it.</p>
+      <p>We believe in simple products that are easy to wear and combine every day.</p>
     </div>
   );
 }
