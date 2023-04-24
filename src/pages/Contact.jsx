6916cd936e060827b5b0eb79b693b3bf377616d4
Nestan <nestankeneshova16@gmail.com>
2023-04-24 19:50:18 +0600

    removed extra paragraphs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,6 @@ export default function Contact(){
   return(
     <div className="Contact">
       <h1>Contacts</h1>
-      <p>This is </p>
-      <p>We only</p>
     </div>
   );
 }
--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -2,8 +2,6 @@ export default function Delivery(){
   return(
     <div className="Delivery">
       <h1>delivery with in week</h1>
-      <p>This is </p>
-      <p>We only</p>
     </div>
   );
 }
