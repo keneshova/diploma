19ccd0286986a46fa8bb75374acfef4f2c8f78fb
Nestan <nestankeneshova16@gmail.com>
2023-04-24 19:43:50 +0600

    fixed bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home(){
   return(
     <div className="Home">
-      <h1>Welcome to Jewelry store</h1>
+      <h1>Welcome to Jewelry Store</h1>
       <p>We will help you find your style and taste</p>
   
     </div>
