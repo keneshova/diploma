f55acb040b90032beaab58027cd9665bbc929e31
Nestan <nestankeneshova16@gmail.com>
2023-04-25 14:27:34 +0600

    created  an order page

---
+++ src/pages/Order.jsx
@@ -0,0 +1,9 @@
+import OrderList from "../componets/OrderList/OrderList";
+export default function Orders() {
+  return(
+    <div className="Orders">
+      <h1>Orders</h1>
+      <OrderList />
+    </div>
+  );
+}
