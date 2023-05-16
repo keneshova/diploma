78cefd3dd34068a47b7e8fe97201c04a441cd5da
Nestan <nestankeneshova16@gmail.com>
2023-05-16 15:27:56 +0600

    added paragraph

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,12 @@ import Vector from "../assets/Vector.png";
 export default function Contact() {
   return (
     <div className="Contact">
-      <h1>Contacts</h1>
+      <div className="contact-menu">
+        <h1>Contact</h1>
+        <p>You can contacts us through this number:</p>
+        <ul><li>700-987-007</li></ul>
+        <ul><li>505-700-904</li></ul>
+      </div>
     </div>
   );
 }
