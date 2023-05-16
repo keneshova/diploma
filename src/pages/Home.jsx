e717522d416e309f7856ea1bd79b2ad899a2682e
Nestan <nestankeneshova16@gmail.com>
2023-05-16 15:27:40 +0600

    added paragraph in home js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ export default function Home(){
        <div className="main-up">
        <h1>Welcome to Jewelry Store</h1>
         <p>We will help you find your style and taste</p>
+        <p>We have a large selection and we will also help you find what you wanted.</p>
        </div>
     </div>
   );
