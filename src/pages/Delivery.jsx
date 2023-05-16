f4555ef6ce1cd46d47999e7b0d0bddce29066643
Nestan <nestankeneshova16@gmail.com>
2023-05-16 16:06:51 +0600

    added parahraphs in classs delivery

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -1,7 +1,13 @@
 export default function Delivery(){
   return(
     <div className="Delivery">
-      <h1>delivery with in week</h1>
+      <div className="delivery-menu">
+      <h1>Delivery</h1>
+      <p>Your delivery will be delivered within a week.</p>
+      <p>Delivery within the city - 50$</p>
+      <p>Delivery by regions and country - 300-500$</p>
+      </div>
+
     </div>
   );
 }
